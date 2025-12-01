1c8d9d561ab4ebfc29a871df5cacaf00df117e77
Cait Roach <155276726+caitroach@users.noreply.github.com>
2025-12-01 13:02:56 -0500

    hello world program

---
+++ testing.asm
@@ -0,0 +1,21 @@
+; hello world 
+; at least trying. help me...
+
+section .text
+global _start 
+
+_start: 
+    mov edx, len 
+    mov ecx, msg 
+
+    mov ebx, 1
+
+    mov eax, 4
+    int 0x80
+
+    mov eax, 1
+    int 0x80
+
+section .data
+    msg db "Hello World :3"
+    len equ $ - msg
